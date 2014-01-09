5537f532e38b12cff6768f9754b61cc03730e27f
Jose Luis Rivas <me@ghostbar.co>
2014-01-09 03:58:17 -0430

    install.sh: Better directory handling

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
-[ -d ~/.vim ] && mv ~/.vim ~/.vim.bak
+[ -e ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
+[ -e ~/.vim.bak ] && rm -rf ~/.vim.bak
+[ -e ~/.vim ] && mv ~/.vim ~/.vim.bak
 
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim ~/.vim
