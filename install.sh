3b75af2f1f4ab23a73f3bed36240d29e622a87aa
Jose Luis Rivas <me@ghostbar.co>
2014-04-23 02:33:38 -0500

    install: typo

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim ~/.vim
 
 echo "Symlinked! Now will clone vundle"
-git clone https://github.com/gmarik/vundle.git ./vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 echo "Now let's gonna install them!"
 vim +BundleInstall +qall
